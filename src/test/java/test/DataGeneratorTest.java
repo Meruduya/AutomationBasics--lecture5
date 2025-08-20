312940e8cbf9056cabe69547254868c378d17d32
Veronika <korynaitroyd@gmail.com>
2025-08-20 19:09:56 +0300

    README

--- src/test/java/test/DataGeneratorTest.java
+++ src/test/java/test/DataGeneratorTest.java
@@ -17,7 +17,7 @@ class DataGeneratorTest {
         // Простые проверки
         assert user.getCity() != null && !user.getCity().isEmpty();
         assert user.getName() != null && !user.getName().isEmpty();
-        assert user.getPhone() != null && !user.getPhone().startsWith("+7");
+        assert user.getPhone() != null && user.getPhone().startsWith("+7");  // ИСПРАВИЛИ ТУТ!
         assert date != null && date.matches("\\d{2}\\.\\d{2}\\.\\d{4}");
     }
 }
